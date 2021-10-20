69b0a27c1d62e878c8c23fe16d94229e71c89d45
yaoxilong <1299583049@qq.com>
2021-10-20 20:38:03 +0800

    【ADD】引入element-ui

--- src/pages/projectA/App.vue
+++ src/pages/projectA/App.vue
@@ -3,13 +3,36 @@
     <div id="nav">
       <router-link to="/">主页</router-link> |
       <router-link to="/about">关于</router-link>
+          <div>
+      <el-button type="primary">主要按钮</el-button>
+    </div>
+        <div>
+    <el-button @click="show = !show">Click Me</el-button>
+
+    <div style="display: flex; margin-top: 20px; height: 100px;">
+      <transition name="el-fade-in-linear">
+        <div v-show="show" class="transition-box">.el-fade-in-linear</div>
+      </transition>
+      <transition name="el-fade-in">
+        <div v-show="show" class="transition-box">.el-fade-in</div>
+      </transition>
+    </div>
+  </div>
     </div>
     <router-view />
 
     <a href="test.html" target="_blank">go to test page</a>
   </div>
-</template>
 
+</template>
+<script>
+    export default {
+    data: () => ({
+      show: true,
+      value: true
+    })
+  }
+</script>
 <style lang="stylus">
 #app
   font-family Avenir, Helvetica, Arial, sans-serif
@@ -18,4 +41,16 @@
   text-align center
   color #2c3e50
   margin-top 60px
+    .transition-box {
+    margin-bottom: 10px;
+    width: 200px;
+    height: 100px;
+    border-radius: 4px;
+    background-color: #409EFF;
+    text-align: center;
+    color: #fff;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    margin-right: 20px;
+  }
 </style>
